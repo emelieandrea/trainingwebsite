c36576d79ff85b2c8848ceff6597cf4d5d208fbf
Emelie <emegu092@student.liu.se>
2024-09-06 21:58:00 +0200

    saved the firebase variables differently

--- trainingwebsite/firebase.ts
+++ trainingwebsite/firebase.ts
@@ -2,19 +2,20 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB370DdU7nYesHsDC7llit0zp8qjj3D63Y",
-    authDomain: "training-database-a5ba0.firebaseapp.com",
-    projectId: "training-database-a5ba0",
-    storageBucket: "training-database-a5ba0.appspot.com",
-    messagingSenderId: "555922962179",
-    appId: "1:555922962179:web:f1358b5610320df30d1b36",
-    measurementId: "G-X4YLK7PC0L"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -22,6 +23,7 @@ const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
+export const auth = getAuth(app);
 export { db };
 
 export default app;
